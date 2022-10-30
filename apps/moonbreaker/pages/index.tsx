6b316a77d650aee2d9e16af621955efe1396d831
shooshte <miha.sustersic.work@gmail.com>
2022-10-30 20:47:54 +0100

    added basic layout styles for roster list

--- apps/moonbreaker/pages/index.tsx
+++ apps/moonbreaker/pages/index.tsx
@@ -3,26 +3,30 @@ import { getRostersList } from '../lib/db/roster';
 import type { GetServerSidePropsContext } from 'next';
 import type { RosterListData } from '../lib/types/roster';
 
+import styles from './index.module.scss';
+
 interface Props {
   rostersList: RosterListData[];
 }
 
 const RostersList = ({ rostersList }: Props) => {
   return (
-    <section>
+    <section className={styles.container}>
       <h1 className="heading-2">Saved rosters:</h1>
 
       {rostersList.map(({ id, name, units }) => {
         return (
-          <section key={`roster-${id}`}>
-            <p className="heading-3">Roster score</p>
-            <p className="heading-3">{units.captain}</p>
-            <p className="heading-3">{name}</p>
-            {/* {units.crew.map((crew) => (
-              <li className="text" key={`crew-${crew}`}>
-                {crew}
-              </li>
-            ))} */}
+          <section className={styles.rosterContainer} key={`roster-${id}`}>
+            <p className={`heading-3 ${styles.score}`}>Roster score</p>
+            <p className={`heading-3 ${styles.captain}`}>{units.captain}</p>
+            <p className={`heading-3 ${styles.name}`}>{name}</p>
+            <ul className={styles.crew}>
+              {units.crew.map((crew) => (
+                <li className="text" key={`crew-${crew}`}>
+                  {crew}
+                </li>
+              ))}
+            </ul>
           </section>
         );
       })}
