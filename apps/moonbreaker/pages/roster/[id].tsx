3f4d4e1ed4d87ff6def2cccc3f91ec8298fd3b30
shooshte <miha.sustersic.work@gmail.com>
2022-11-02 13:02:14 +0100

    WIP: Fixing build in-between develop deploys to get e2e tests up and running.

--- apps/moonbreaker/pages/roster/[id].tsx
+++ apps/moonbreaker/pages/roster/[id].tsx
@@ -5,9 +5,11 @@ const Roster = () => <div>Roster info placeholder.</div>;
 export default Roster;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const rostersList = await getRostersList({
-    patchName: 'Pre-Alpha 39919',
-  });
+  // const rostersList = await getRostersList({
+  //   patchName: 'Pre-Alpha 39919',
+  // });
+
+  const rostersList = [];
 
   return {
     props: {
